cbedb0984dd564d2992aba08270661fe05db8310
Amrabdo74 <andkpe12@gmail.com>
2021-12-07 09:55:01 +0200

    cv

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import img1 from './img/amr.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
+function App() {
+  return (
+    <div className="App">
+      <div class="content">
+    <div class="one">
+        <div class="section-one">
+        <img src={img1} alt="" />
+
+        <h2>Amr abdo</h2>
+        <h4>
+       <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>
+
+         tala</h4>
+        <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i> amrabdo@gmail.com</h4>
+        <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i> 01021798849</h4>
+        
+    </div>
+    <div class="section-two">
+       <h4> <i class="fab fa-angellist"></i> skills</h4>
+       <h5>Html</h5>
+      
+       
+       <h4 class="space"> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>language</h4>
+       <h5>Arbic</h5>
+       <h5>English</h5>
+       
+    </div>
+    </div>
+    <div class="two">
+        <div class="top">
+            <h1> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i> Work experience</h1>
+            <h4> Front-end develper</h4>
+            <p>Hello World , I'm Computer Science Student , interested in some CS branches like programming and web development . aims to Be a software engineer , I have a good knowledge of HTML & CSS , JavaScript</p>
+            <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>jan 2019</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga laborum et iure corrupti, explicabo cupiditate dolor blanditiis pariatur, delectus quidem illo ea. Laudantium totam veritatis ipsum non neque quibusdam.</p>
+            <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>may 2020</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga laborum et iure corrupti, explicabo cupiditate dolor blanditiis pariatur, delectus quidem illo ea. Laudantium totam veritatis ipsum non neque quibusdam.</p>
+            <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>Abril 2021</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga laborum et iure corrupti, explicabo cupiditate dolor blanditiis pariatur, delectus quidem illo ea. Laudantium totam veritatis ipsum non neque quibusdam.</p>
+      
+        </div>
+        <div class="down">
+            <h1> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>Education</h1>
+            <h4> W3schools.com</h4>
+            <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>forever</h4>
+            <p>Web Development! All need to know in one place</p>
+            <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>may 2020</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga laborum et iure corrupti, explicabo cupiditate dolor blanditiis pariatur, delectus quidem illo ea. Laudantium totam veritatis ipsum non neque quibusdam.</p>
+            <h4> <i> <FontAwesomeIcon icon={faSlidersH} className="" /></i>Abril 2021</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fuga laborum et iure corrupti, explicabo cupiditate dolor blanditiis pariatur, delectus quidem illo ea. Laudantium totam veritatis ipsum non neque quibusdam.</p>
+      
+        </div>
+    </div>
+</div>
+<div class="footer">
+<a target="_blank" href="https://www.facebook.com/profile.php?id=100007260193308">facebook</a> 
+<a target="_blank" href="https://www.facebook.com/profile.php?id=100007260193308">instagram</a> 
+<a target="_blank" href="https://www.facebook.com/profile.php?id=100007260193308">linkedin</a> 
+<a target="_blank" href="https://www.facebook.com/profile.php?id=100007260193308">behance</a> 
+    <div class="right"><p>Copyright © 2020 All rights reserved | made by <a href="https://www.linkedin.com/in/Amr-abdo74/?fbclid=IwAR04uXxsnt31pYjwLWZdib0hwHQGQB-7dpOb-RTfQFAp4zmXn8jujv4QWxk"> Amr mohamed</a></p></div>
+
+</div>
+    </div>
+  );
+}
+
+export default App;
